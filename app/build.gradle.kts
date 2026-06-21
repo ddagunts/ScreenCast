7e66bbe7ae5cc64c012bbe4987940be67925d137
Dmitry Dagunts <ddagunts@gmail.com>
2026-06-21 08:40:42 -0400

    0.14.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "io.github.ddagunts.screencast"
         minSdk = 26
         targetSdk = 36
-        versionCode = 30
-        versionName = "0.14.0"
+        versionCode = 31
+        versionName = "0.14.1"
     }
 
     signingConfigs {
